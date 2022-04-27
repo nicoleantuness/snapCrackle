8503eea3a521a18c0a0aee214f66a46f1e8e0bc0
Nicole Antunes <102484958+nicoleantuness@users.noreply.github.com>
2022-04-27 09:30:57 -0300

    Add files via upload

---
+++ snapCrackle.js
@@ -0,0 +1,19 @@
+function snapCrackle(maxValue) {
+    let result = ''
+    for (let i = 1; i <= maxValue; i++) {
+    if (i % 2 !== 0 && i % 5 === 0) {
+        result +='SnapCrackle, '
+            continue
+        }
+    if (i % 2 !== 0) {
+         result += 'Snap, '
+            continue
+        }
+     if (i % 5 === 0) {
+        result += 'Crackle, '
+            continue
+        }
+        result += i + ', '
+    }
+    return result
+}
